933fce165e7079afa091bb02d22e4f32ae561e61
praveen-betalectic <102508587+praveen-betalectic@users.noreply.github.com>
2023-02-20 11:53:32 +0530

    scheduled worker implemented

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first scheduled worker.
+ *
+ * - Run `wrangler dev --local` in your terminal to start a development server
+ * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
+ * - Go back to the console to see what your worker has logged
+ * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
+ * - Run `wrangler publish --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
+ */
+
+// export default {
+// 	async scheduled(controller, env, ctx) {
+// 		console.log(`Hello World!`);
+// 	},
+// };
+
+addEventListener(
+  "scheduled",
+  (event) => {
+    event.waitUntil(runScheduledTask());
+  },
+  { cron: "* * * * *" }
+);
+
+async function runScheduledTask() {
+  console.log("Scheduled task ran at " + new Date());
+}
